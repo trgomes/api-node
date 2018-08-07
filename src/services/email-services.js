fd682722f06593f53aeb290a0bde3c4029be6cee
trtg <trtg10@gmail.com>
2018-08-07 19:48:52 -0300

    update

---
+++ src/services/email-services.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const config = require('../config');
+const sendgrid = require('sendgrid')(config.sendgridKey);
+
+exports.send = async(to, subject, body) => {
+    sendgrid.send({
+        to: to,
+        from: 'hello@gmail.com',
+        subject: subject,
+        html: body
+    });
+};
